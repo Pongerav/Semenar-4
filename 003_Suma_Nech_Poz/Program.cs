47eade0667dc6cc26668be2e89923d435ea66430
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-08-28 11:42:27 +0300

    Added third ex

---
+++ 003_Suma_Nech_Poz/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArrayRandom(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,100);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.WriteLine("Введите длину массива");
+int length = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[length];
+FillArrayRandom(array);
+PrintArray(array);
+
+int sum = 0;
+
+for (int i = 1; i < length; i += 2) sum += array[i];
+
+Console.WriteLine($"\n{sum} - сумма чисел на нечётных позициях");
