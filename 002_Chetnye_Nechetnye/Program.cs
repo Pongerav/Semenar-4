d1366fa0beabc09a28a8a0f9c3ae9ec09188b366
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-08-28 11:15:10 +0300

    Added secont excercise

---
+++ 002_Chetnye_Nechetnye/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArrayRandom(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+int Chetn(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) result += 1;
+    }
+    return result;
+}
+
+
+int NChetn(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 1) result += 1;
+    }
+    return result;
+}
+
+
+Console.WriteLine("Введите длину массива");
+int length = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[length];
+
+FillArrayRandom(array);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine($"{Chetn(array)} - чётных элементов в массиве  " + $"\n{NChetn(array)} - нечётных элементов в массиве");
