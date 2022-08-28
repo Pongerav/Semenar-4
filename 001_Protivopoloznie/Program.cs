6ee4096d9c2a7e56bf975f4a30b2ee655f12b32e
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-08-28 10:44:49 +0300

    Added first ex

---
+++ 001_Protivopoloznie/Program.cs
@@ -0,0 +1,32 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива {i+1}: ");
+        array[i] = int.Parse(Console.ReadLine() ?? "0");
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+Console.WriteLine("Введите длину массива");
+
+int length = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[length];
+
+FillArray(array);
+
+for (int i = 0; i < length; i++)
+{
+    array[i] = array[i] * (-1);
+}
+
+PrintArray(array);
